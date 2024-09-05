feca4fc5bfce0f708f9c3a372bbf69e24927795f
arielcamposchavarria <campos2004.05@gmail.com>
2024-09-04 18:44:02 -0600

    Concesiones

--- src/SolicitudConcesion/Concesion.service.ts
+++ src/SolicitudConcesion/Concesion.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Concesion } from './Concesion.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class ConcesionesService {
+export class ConcesionService {
+  constructor(
+    @InjectRepository(Concesion)
+    private readonly concesionRepository: Repository<Concesion>,
+  ) {}
+  async getAllConcesiones(): Promise<Concesion[]> {
+    return await this.concesionRepository.find();
+  }
+
   // Aquí puedes añadir lógica adicional como guardar la ruta del archivo en la base de datos
-  handleFile() {
+  async handleFile() {
     // Lógica para manejar el archivo subido
   }
 }
